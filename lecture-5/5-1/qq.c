7278b33d2a5931cf4fd27a78696006668b3aed37
SamYoung <yangg052@gmail.com>
2018-05-28 14:18:55 +0800

    Create qq.c

---
+++ lecture-5/5-1/qq.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "string.h"
+
+int main()
+{
+	char passwd[128] = {0};
+	char number[128] = {0};
+	char path[128] = "./save.txt";
+	int f_id;
+	printf("please input qq code\n");
+	scanf("%s",number);
+	printf("please input passwd\n");
+	scanf("%s",passwd);
+	printf("start login.........\n");
+	while(1);
+	return 0;
+}
